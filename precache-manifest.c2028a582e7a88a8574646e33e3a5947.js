5c5ebae6c987871d23dd6576dc5584aa40ba2613
kanweitech <kanweiedward>
2022-04-14 21:35:19 +0100

    Updates

---
+++ precache-manifest.c2028a582e7a88a8574646e33e3a5947.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31534633137c17c70244",
+    "url": "/static/css/main.53668e8c.chunk.css"
+  },
+  {
+    "revision": "31534633137c17c70244",
+    "url": "/static/js/main.31534633.chunk.js"
+  },
+  {
+    "revision": "080cfc70864c55772213",
+    "url": "/static/js/1.080cfc70.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5e270daf7c2b6502dfcf0bb34abb88c4",
+    "url": "/index.html"
+  }
+];
