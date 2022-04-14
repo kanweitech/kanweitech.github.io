d7557f1c203d069ab30bbb268b539211f10d6c43
kanweitech <kanweiedward>
2022-04-14 21:31:22 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App'
+// import * as ReactDOMClient from 'react-dom/client';
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// Create a root.
+// const container = ReactDOMClient.createRoot(App);
+// ReactDOMClient.createRoot(<App />, document.getElementById('root'))
